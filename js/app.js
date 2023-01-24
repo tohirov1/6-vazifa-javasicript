dac231c4ddbdf442920a864d98024a21460f7857
Your Name <you@example.com>
2023-01-24 18:11:13 +0500

    git commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+
+
+// 2 vazifa
+// let nums = [1,3,2,4,5,6,99,102]
+// let target = 5
+// let num = nums.sort(function(a,b){return a -b});
+// let count = num.indexOf(target)
+// console.log(count);
+
+// 2 - masala
+// let nums = [8,7,7,8,8,8,8,10];
+// let target = 8;
+// let tanlangan = [];
+
+// for (let i = 0 ; i < nums.length ; i++){
+//     if(nums[i] === target){
+//         let a = nums.indexOf(target);
+//         nums.splice(a,1,3);
+//         tanlangan.push(a);           
+//     }    else if ((tanlangan.length) === 0){
+//         console.log(`${target} bu son mavjud emas`);
+//     }
+// }
+
+// console.log(tanlangan);  
+
+// 3 masala
+// let number =  [4,5,6,7,3,1,0];
+// let target = 0
+
+// for (let i = 0; i < number.length; i++) {
+//     const element = number[i];
+    
+//     if(element === target){
+//         console.log(number.indexOf(element));
+//     }
+// }
+
+
+
+// 4 masala
+// let nums = [4,5,6,9]
+// let tar = 9
+
+
+// for (let i = 0; i < nums.length; i++) {
+
+//    for (let j = i + 1; j < nums.length; j++) {
+
+//     if(nums[i] + nums[j] == tar){
+//         console.log(i, j)
+//     }
+//    }
+// }
+
+
+ 
